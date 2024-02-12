55c559b5207e3ad61edfb3c03e54ccf3a1c549b4
vrajesh0167 <chovatiyavrajesh167@gmail.com>
2024-02-12 16:55:02 +0530

    fix firebase api key

--- client/src/Firebase.jsx
+++ client/src/Firebase.jsx
@@ -6,7 +6,7 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: import.meta.env.FIREBASE_API_KEY,
+    apiKey: "AIzaSyClERao002GA2CJ6vxcRoDn-UfUHF0Vf6U",
     authDomain: "skyline-estate-e603b.firebaseapp.com",
     projectId: "skyline-estate-e603b",
     storageBucket: "skyline-estate-e603b.appspot.com",
